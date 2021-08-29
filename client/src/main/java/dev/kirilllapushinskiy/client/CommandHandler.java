31f2091654379de46b41da9f148870580b6aafc8
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-29 13:22:42 +0300

    Added registration method.

--- client/src/main/java/dev/kirilllapushinskiy/client/CommandHandler.java
+++ client/src/main/java/dev/kirilllapushinskiy/client/CommandHandler.java
@@ -1,7 +1,9 @@
 package dev.kirilllapushinskiy.client;
 import dev.kirilllapushinskiy.commands.Command;
+import dev.kirilllapushinskiy.commands.CommandPackage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,5 +14,14 @@ public class CommandHandler {
     static private final Scanner scanner = new Scanner(System.in);
 
     static private final List<Command> commands = new ArrayList<Command>(10);
+
+    public static void registration(Command... commands) {
+        for (Command c : commands)
+            if (CommandHandler.commands.contains(c)) {
+                throw new IllegalStateException(c+ "already in commands!");
+            } else {
+                CommandHandler.commands.add(c);
+            }
+    }
 
 }
