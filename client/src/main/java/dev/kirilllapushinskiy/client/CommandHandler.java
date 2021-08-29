3a463472ee42e77d368ac2c36f2d2e67003fe768
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-29 19:04:30 +0300

    Scan and parse functions moved into new class.

--- client/src/main/java/dev/kirilllapushinskiy/client/CommandHandler.java
+++ client/src/main/java/dev/kirilllapushinskiy/client/CommandHandler.java
@@ -1,16 +1,10 @@
 package dev.kirilllapushinskiy.client;
 import dev.kirilllapushinskiy.commands.Command;
 import dev.kirilllapushinskiy.commands.CommandCollection;
-import dev.kirilllapushinskiy.commands.CommandPackage;
-
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class CommandHandler {
 
     static private final String EXIT = "exit";
-
-    static private final Scanner scanner = new Scanner(System.in);
 
     static private final CommandCollection commands = new CommandCollection(10);
 
@@ -26,20 +20,30 @@ public class CommandHandler {
     public static void start() {
         if (commands.isEmpty())
             throw new IllegalStateException("No one command had been registered!");
-        String line;
+
         while (true) {
             System.out.print("Enter command: ");
-            line = scanner.nextLine();
-            System.out.println("Read: " + line + ".");
-            if (line.equalsIgnoreCase(EXIT)) {
-                break;
-            } else if (commands.conclude(line)) {
-                CommandPackage p = commands.getCommandPackage(line);
-                System.out.println(p.getPackedCommandName());
-                System.out.println(Arrays.toString(p.getPackedCommandArgs()));
+            if (!CommandParser.readCommand()) continue;
+            if (CommandParser.commandEquals(EXIT)) break;
+
+            if (commands.conclude(CommandParser.getCommandName())) {
+                Command c = commands.getCommand(CommandParser.getCommandName());
+                if (c.requiredArgs()) {
+                    if (CommandParser.withArgs()) {
+                        c.setCommandArgs(CommandParser.getCommandArgs());
+                    } else {
+                        System.out.println("Command require arguments!");
+                    }
+                } else if (CommandParser.withArgs()) {
+                    c.setCommandArgs(CommandParser.getCommandArgs());
+                }
+
+                //CommandPackage pack = CommandPackage.pack(c);
+
             } else {
-                System.out.println("Command '" + line + "' not registered!");
+                System.out.println("Command '" + CommandParser.getCommandName() + "' not recognized!");
             }
+
         }
         System.out.println("Bye-bye!");
     }
