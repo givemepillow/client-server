0f3e1e543aff3fc125e02414016f57dc2554ae71
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-30 21:16:23 +0300

    New start() implementation.

--- client/src/main/java/dev/kirilllapushinskiy/handlers/CommandHandler.java
+++ client/src/main/java/dev/kirilllapushinskiy/handlers/CommandHandler.java
@@ -1,49 +1,54 @@
 package dev.kirilllapushinskiy.handlers;
+
 import dev.kirilllapushinskiy.commands.Command;
 import dev.kirilllapushinskiy.commands.CommandCollection;
 
 public class CommandHandler {
-
-    static private final String EXIT = "exit";
 
     static private final CommandCollection commands = new CommandCollection(10);
 
     public static void registration(Command... commands) {
         for (Command c : commands)
             if (CommandHandler.commands.contains(c)) {
-                throw new IllegalStateException(c+ "already in commands!");
+                throw new IllegalStateException(c + "already in commands!");
             } else {
                 CommandHandler.commands.add(c);
             }
     }
 
-    public static void start() {
+    public static Command start() {
+
         if (commands.isEmpty())
             throw new IllegalStateException("No one command had been registered!");
 
         while (true) {
             System.out.print("Enter command: ");
             if (!CommandParser.readCommand()) continue;
-            if (CommandParser.commandEquals(EXIT)) break;
 
             if (commands.conclude(CommandParser.getCommandName())) {
+
                 Command c = commands.getCommand(CommandParser.getCommandName());
+
                 if (c.requiredArgs()) {
+
                     if (CommandParser.withArgs()) {
                         c.setCommandArgs(CommandParser.getCommandArgs());
                     } else {
                         System.out.println("Command require arguments!");
+                        continue;
                     }
+
                 } else if (CommandParser.withArgs()) {
                     c.setCommandArgs(CommandParser.getCommandArgs());
                 }
 
+                return c;
+
             } else {
+
                 System.out.println("Command '" + CommandParser.getCommandName() + "' not recognized!");
+
             }
-
         }
-        System.out.println("Bye-bye!");
     }
-
 }
