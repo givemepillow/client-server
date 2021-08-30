895f6d0a07f1048f2eaa7a5aeba66e0a0924228b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-30 15:44:35 +0300

    Added getMessage() method.

--- sharedlib/src/main/java/dev/kirilllapushinskiy/communication/Message.java
+++ sharedlib/src/main/java/dev/kirilllapushinskiy/communication/Message.java
@@ -4,4 +4,8 @@ abstract class Message {
 
     protected String data;
 
+    protected String getMessage() {
+        if (data == null) throw new IllegalStateException("Empty message!");
+        return this.data;
+    }
 }
