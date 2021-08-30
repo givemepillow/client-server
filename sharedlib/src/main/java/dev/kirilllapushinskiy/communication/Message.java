b4d303c499be606a6b68c67d325f4afc443d65a6
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-30 15:48:46 +0300

    Added protected constructor.

--- sharedlib/src/main/java/dev/kirilllapushinskiy/communication/Message.java
+++ sharedlib/src/main/java/dev/kirilllapushinskiy/communication/Message.java
@@ -4,6 +4,11 @@ abstract class Message {
 
     protected String data;
 
+    protected Message(String message) {
+        if (message == null) throw new IllegalArgumentException("Null message!");
+        this.data = message;
+    }
+
     protected String getMessage() {
         if (data == null) throw new IllegalStateException("Empty message!");
         return this.data;
