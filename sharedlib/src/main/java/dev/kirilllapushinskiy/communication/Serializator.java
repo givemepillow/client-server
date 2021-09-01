ec88381188c81bcb10c6faa912c88c95978dc871
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-01 12:33:12 +0300

    Serialization methods moved into new class

---
+++ sharedlib/src/main/java/dev/kirilllapushinskiy/communication/Serializator.java
@@ -0,0 +1,24 @@
+package dev.kirilllapushinskiy.communication;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public final class Serializator {
+
+    private Serializator() {}
+
+    static public ByteBuffer serialize(Serializable object) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(bos);
+        out.writeObject(object);
+        out.flush();
+        return ByteBuffer.wrap(bos.toByteArray());
+    }
+
+    static public Object deserialize(ByteBuffer buffer) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(buffer.array());
+        ObjectInputStream in = new ObjectInputStream(bis);
+        return in.readObject();
+    }
+
+}
