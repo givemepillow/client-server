82bb21e6c7e489fcb6a2416c45a127c05e0f6701
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-02 14:55:20 +0300

    ServerCommandHandler init version.

---
+++ server/src/main/java/dev/kirilllapushinskiy/core/ServerCommandHandler.java
@@ -0,0 +1,18 @@
+package dev.kirilllapushinskiy.core;
+
+import dev.kirilllapushinskiy.commands.Command;
+import dev.kirilllapushinskiy.commands.CommandPackage;
+import dev.kirilllapushinskiy.commands.CommandRegistrator;
+
+public class ServerCommandHandler extends CommandRegistrator {
+
+
+    public static Command handle(CommandPackage commandPackage) {
+
+        if (!commands.conclude(commandPackage.getPackedCommandName()))
+            throw new IllegalArgumentException("Undefined command!");
+
+        return commands.getCommand(commandPackage.getPackedCommandName());
+    }
+}
+
