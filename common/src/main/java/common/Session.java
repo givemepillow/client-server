46dfeefa4aae392982d5c33929f2487852f63643
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-23 21:06:39 +0300

    Support for HumanBeing and History.

--- common/src/main/java/common/Session.java
+++ common/src/main/java/common/Session.java
@@ -2,4 +2,8 @@ package common;
 
 public interface Session {
     void setResponse(Response response);
+    HumanBeing getHumanBeing();
+    void setHumanBeing(HumanBeing hb);
+    String getHistory();
+    void setHistory(Command command);
 }
