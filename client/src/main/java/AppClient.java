403ca0eba5ebd1ad2718a9c6165c2d8f99c1a0c1
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-23 21:04:04 +0300

    New commands registration.

--- client/src/main/java/AppClient.java
+++ client/src/main/java/AppClient.java
@@ -7,7 +7,19 @@ public class AppClient {
         CommandHandler.registration(
                 HELP,
                 EXIT,
-                CLEAR
+                CLEAR,
+                ADD,
+                SHOW,
+                HISTORY,
+                UPDATE,
+                INFO,
+                REMOVEBYID,
+                UNIQUEWEAPON,
+                MAXSOUNTRACK,
+                FILTERNAME,
+                ADDIFMIN,
+                ADDIFMAX,
+                EXECUTESCRIPT
         );
         CommandHandler.start();
 
@@ -16,4 +28,16 @@ public class AppClient {
     final static Command HELP = new Help();
     final static Command EXIT = new Exit();
     final static Command CLEAR = new Clear();
+    final static Command ADD = new Add();
+    final static Command SHOW = new Show();
+    final static Command HISTORY = new History();
+    final static Command UPDATE = new Update();
+    final static Command INFO = new Info();
+    final static Command REMOVEBYID = new RemoveById();
+    final static Command UNIQUEWEAPON = new UniqueWeapon();
+    final static Command MAXSOUNTRACK = new MaxSoundtrack();
+    final static Command FILTERNAME = new FilterName();
+    final static Command ADDIFMIN = new AddIfMin();
+    final static Command ADDIFMAX = new AddIfMax();
+    final static Command EXECUTESCRIPT = new ExecuteScript();
 }
--- server/src/main/java/AppServer.java
+++ server/src/main/java/AppServer.java
@@ -6,11 +6,24 @@ public class AppServer {
 
     public static void main(String[] args) {
         try {
-
+            Storage.init();
 
             Server.init();
             CommandHandler.registration(
-                    HELP
+                    HELP,
+                    ADD,
+                    SHOW,
+                    HISTORY,
+                    CHECKID,
+                    UPDATE,
+                    INFO,
+                    REMOVEBYID,
+                    UNIQUEWEAPON,
+                    CLEAR,
+                    MAXSOUNTRACK,
+                    FILTERNAME,
+                    ADDIFMIN,
+                    ADDIFMAX
             );
             Server.start();
 
@@ -20,5 +33,17 @@ public class AppServer {
     }
 
     final static Command HELP = new Help();
-
+    final static Command ADD = new Add();
+    final static Command SHOW = new Show();
+    final static Command UPDATE = new Update();
+    final static Command HISTORY = new History();
+    final static Command CHECKID = new CheckId();
+    final static Command INFO = new Info();
+    final static Command REMOVEBYID = new RemoveById();
+    final static Command UNIQUEWEAPON = new UniqueWeapon();
+    final static Command CLEAR = new Clear();
+    final static Command MAXSOUNTRACK = new MaxSoundtrack();
+    final static Command FILTERNAME = new FilterName();
+    final static Command ADDIFMIN = new AddIfMin();
+    final static Command ADDIFMAX = new AddIfMax();
 }
