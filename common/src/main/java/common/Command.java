bf6615924d20dcfaf4cdd116a8253ccb6d1311c6
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-23 21:00:36 +0300

    Added support for HumanBeing and internal commands.

--- common/src/main/java/common/Command.java
+++ common/src/main/java/common/Command.java
@@ -5,6 +5,10 @@ public interface Command {
     String getCommandArgument();
     void setCommandArgument(String commandArgument);
     boolean isRequiredArgument();
+    boolean isInternal();
     default void run() {}
     default void run(Session session) {}
+    boolean isRequiredHumanBeing();
+    HumanBeing getHumanBeing();
+    void setHumanBeing(HumanBeing hb);
 }
