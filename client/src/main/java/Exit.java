5fa175c33307d8239acc6fdda95abc91235c4b4f
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-24 09:35:12 +0300

    Message if server not response on client exit.

--- client/src/main/java/Exit.java
+++ client/src/main/java/Exit.java
@@ -11,6 +11,8 @@ public class Exit extends AbstractCommand {
     public void run() {
         Response response = Communicator.remoteCommandExecution(this);
         System.out.println(response);
+        if (response.getData().equalsIgnoreCase("Сервер не доступен, попробуйте позже... :("))
+            System.out.println("Ваши изменения на сервере не были сохранены.");
         System.out.println("\nЗавершение работы программы...");
         System.exit(0);
     }
